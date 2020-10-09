34a672891e807b9d03f41d0cbf85057db1a4506a
talltotal <talltotal@163.com>
2020-10-09 14:24:07 +0800

    update jsdoc

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 /**
- * 通过key-value形式配置mock数据
+ * @author talltotal
+ * @overview 通过key-value形式配置mock数据
  * - key: `${method}|${dalay} ${path/regexp}`
  * - value:
- *  - mockData
- *  - funtion ({ params: [...path/regexp], ...query, ...body }) { return mockData || function (response) {} }
+ *    - mockData
+ *    - funtion ({ params: [...path/regexp], ...query, ...body }) { return mockData || function (response) {} }
  * 
  * 处理过程
  * - 文件夹内任何变动都重新加载api
  * - 文件中都kv转变格式用于处理
- *  - { [method]: { [path/regexp]: { dalay, value } } }
+ *    - { [method]: { [path/regexp]: { dalay, value } } }
  * - 匹配([method]~[path/regexp])并处理({ dalay, value })请求
  */
+
+/**
+ * @module Express_kv_api
+ */
+
 const path = require('path')
 const process = require('process')
 const fs = require('fs')
@@ -20,7 +26,31 @@ const Mock = require('mockjs')
 const { pathToRegexp } = require('path-to-regexp')
 const glob = require('glob')
 
-/** 全局配置 */
+/**
+ * @callback DataWrap
+ * @param {*} resData 响应数据
+ * @returns {*}
+ */
+
+/**
+ * @callback ReqDataWrap
+ * @param {Request} req 请求对象
+ * @param {Array} params 请求path中的参数
+ * @returns {Object}
+ */
+
+/**
+ * @typedef {Object} Options
+ * @property {string} dirPath 存放接口配置的文件夹路径
+ * @property {DataWrap} dataWrap 响应数据处理函数
+ * @property {string} [defaultMathod=all] 默认请求mathod
+ * @property {boolean} [moduleByPath=false] 以文件路径为请求路径
+ * @property {ReqDataWrap} reqDataWrap 请求数据处理函数
+ */
+
+/** 全局配置
+ * @type {Options}
+ */
 const Options = {
   dirPath: '',
   dataWrap: (data) => data,
@@ -38,10 +68,11 @@ const Options = {
   },
 }
 
+/** @namespace */
 const Key = {
   /**
-   * 解析
-   * @param {string} str 
+   * 解析Key
+   * @param {string} str key字符串
    * @returns {{method: string, delay: number, path: string}}
    */
   parse (str) {
@@ -67,6 +98,10 @@ const Key = {
   },
 }
 
+/**
+ * 从文件获取数据重置接口对象
+ * @param {Object} apiObj 接口对象
+ */
 function resetApisFromDir (apiObj) {
   const dir = Options.dirPath
   glob(path.join(dir, '**/*.{json,js}'), (err, files) => {
@@ -81,9 +116,11 @@ function resetApisFromDir (apiObj) {
 }
 
 /**
- * @param {Request} req 
- * @param {Response} res 
- * @param {Object} apiObj 
+ * 匹配并处理接口
+ * @param {Request} req 请求
+ * @param {Response} res 响应
+ * @param {Object} apiObj 接口对象
+ * @returns {boolean}
  */
 function matchApiAndHandle (req, res, apiObj) {
   const method = req.method.toLowerCase()
@@ -110,6 +147,11 @@ function matchApiAndHandle (req, res, apiObj) {
   return false
 }
 
+/**
+ * 从文件获取配置，往接口对象增加数据
+ * @param {Object} apiObj 接口对象
+ * @param {string} apiFilePath 接口配置文件路径
+ */
 function addApisFromFile (apiObj, apiFilePath) {
   const relativePath = path.relative(Options.dirPath, apiFilePath)
   const isHideFile = !!relativePath.match(/(\/\.)|(\\\.)/g)
@@ -134,6 +176,12 @@ function addApisFromFile (apiObj, apiFilePath) {
   }
 }
 
+/**
+ * 解析配置格式为易匹配的格式，转存到目标对象中
+ * @param {Object} target 目标对象
+ * @param {Object} source 源对象
+ * @param  {...string} namespaces 数据的命名空间
+ */
 function addApiFromObj (target, source, ...namespaces) {
   Object.keys(source).forEach((key) => {
     const { method, delay, path: apiPath } = Key.parse(key)
@@ -146,6 +194,16 @@ function addApiFromObj (target, source, ...namespaces) {
   })
 }
 
+/**
+ * 响应请求
+ * @param {Object} param0 配置数据
+ * @param {number} param0.delay 延迟时长
+ * @param {*} param0.value 接口
+ * @param {Object} param1 请求数据
+ * @param {Request} param1.req 请求对象
+ * @param {Object} param1.params 请求参数
+ * @param {Response} res 响应
+ */
 function handleRes ({ delay, value }, { req, params }, res) {
   function main () {
     if (typeof value === 'function') {
@@ -173,14 +231,15 @@ function handleRes ({ delay, value }, { req, params }, res) {
 }
 
 /**
+ * 返回express路由函数
  * @param {object} opts
- * @param {string} [opts.dirPath] server routers' dir path；接口文件夹
- * @param {(data: any) => any} [opts.dataWrap] warp response data；处理数据整体包装函数
- * @param {string} [opts.defaultMathod] default mathod for routers matching；请求的默认方法
- * @param {boolean} [opts.moduleByPath] 以目录结构划分模块
- * @param {boolean} [opts.watch] 是否监听接口文件夹内变动
- * @param {(req: Request, params: Array) => any} [opts.reqDataWrap] warp request data for routers
- * @returns {(req: Request, res: Response, next: Function) => any}
+ * @param {string} [opts.dirPath=server] server routers' dir path；接口文件夹
+ * @param {DataWrap} opts.dataWrap warp response data；处理数据整体包装函数
+ * @param {string} opts.defaultMathod default mathod for routers matching；请求的默认方法
+ * @param {boolean} [opts.moduleByPath=false] 以目录结构划分模块
+ * @param {boolean} [opts.watch=true] 是否监听接口文件夹内变动
+ * @param {ReqDataWrap} opts.reqDataWrap warp request data for routers
+ * @returns {function}
  */
 module.exports = function (opts = {}) {
   const {
